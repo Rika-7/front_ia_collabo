efeb19765ec03c3a04eb8d208568f42249b31a67
Rika <>
2025-04-08 21:10:13 -0400

    Initial commit.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
-export default function Home() {
+export default function Dashboard() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">研Q</h1>
-        <p className="text-lg">研究者と企業をつなぐプラットフォーム</p>
-        <p className="text-lg">産学連携用プラットフォーム</p>
+        <p className="text-lg">ダッシュボード</p>
       </main>
     </div>
   );
